f110611fbeaa03d3369d0dc47a56bd84d7001d0d
songchez <tama4840@gmail.com>
2025-02-07 18:19:57 +0900

    ui변경

--- src/app/myaccount/page.tsx
+++ src/app/myaccount/page.tsx
@@ -3,3 +3,8 @@ import React from "react";
 export default function Myaccount() {
   return <div>myaccount</div>;
 }
+
+// 구독정보 : 얼마나 남았는지 + Free티어일 경우 토큰이 몇개 남았는지, 구독이 얼마나 남았는지
+// 개인정보 : 이름, 카드정보, Schedai를 얼마나 사용했는지,
+// SchedAI소통하기 1:1대화
+// 나만의 프롬프트 추가 : 커스터 마이징
